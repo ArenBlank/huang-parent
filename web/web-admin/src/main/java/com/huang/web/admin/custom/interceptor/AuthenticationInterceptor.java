89bbc8dea179d49c2de1bb9a26f07d0acb275541
ArenBlank <645040170@qq.com>
2026-05-20 18:03:28 +0800

    fix: skip auth for OPTIONS preflight requests to allow CORS to work

--- web/web-admin/src/main/java/com/huang/web/admin/custom/interceptor/AuthenticationInterceptor.java
+++ web/web-admin/src/main/java/com/huang/web/admin/custom/interceptor/AuthenticationInterceptor.java
@@ -42,6 +42,9 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
         String token = request.getHeader("Authorization");
         if (!StringUtils.hasText(token)) {
             token = request.getHeader("access-token");
--- web/web-app/src/main/java/com/huang/web/app/custom/interceptor/AuthenticationInterceptor.java
+++ web/web-app/src/main/java/com/huang/web/app/custom/interceptor/AuthenticationInterceptor.java
@@ -29,6 +29,9 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
         String requestURI = request.getRequestURI();
         log.info("App JWT authentication intercepting request: {}", requestURI);
 
